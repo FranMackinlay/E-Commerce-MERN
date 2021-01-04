31c7a77bce96dd883e27a68d61d9ccb87891dac6
Francisco Mackinlay <franmackinlay@gmail.com>
2021-01-04 16:32:35 -0300

    ADD, react-router-dom & Screens

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -6,10 +6,12 @@ const data = {
       category: 'Shirts',
       image: '/images/p1.jpg',
       price: 120,
+      countInStock: 10,
       brand: 'Nike',
       rating: 4.5,
       numReviews: 10,
       description: 'high quality product',
+
     }
   ]
 };
