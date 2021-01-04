804211753cb33e84593b8110a8b2b614557638e2
Francisco Mackinlay <franmackinlay@gmail.com>
2021-01-04 15:15:42 -0300

    ADD, react app

---
+++ frontend/src/data.js
@@ -0,0 +1,17 @@
+const data = {
+  products: [
+    {
+      _id: '1',
+      name: 'Nike Slim Shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 120,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    }
+  ]
+};
+
+export default data;
