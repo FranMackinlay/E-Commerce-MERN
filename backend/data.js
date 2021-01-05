5a9015b3c8fca68d6b51976d7a69938af0c66967
Francisco Mackinlay <franmackinlay@gmail.com>
2021-01-05 17:05:34 -0300

    POPULATE, db

--- backend/data.js
+++ backend/data.js
@@ -12,7 +12,60 @@ const data = {
       numReviews: 10,
       description: 'high quality product',
 
-    }
+    },
+    {
+      _id: '1',
+      name: 'Adidas Slim Shirt',
+      category: 'Shirts',
+      image: '/images/p2.jpg',
+      price: 80,
+      countInStock: 5,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+
+    },
+    {
+      _id: '2',
+      name: 'Topper Slim Shirt',
+      category: 'Shirts',
+      image: '/images/p3.jpg',
+      price: 110,
+      countInStock: 30,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+
+    },
+    {
+      _id: '3',
+      name: 'Puma Slim Shirt',
+      category: 'Shirts',
+      image: '/images/p4.jpg',
+      price: 180,
+      countInStock: 0,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+
+    },
+    {
+      _id: '4',
+      name: 'Zara Slim Shirt',
+      category: 'Shirts',
+      image: '/images/p5.jpg',
+      price: 100,
+      countInStock: 15,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+
+    },
+
   ]
 };
 
