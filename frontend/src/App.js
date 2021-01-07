cc1704794dbeaa51ce1503a1855dac2dda4ca2aa
Francisco Mackinlay <franmackinlay@gmail.com>
2021-01-07 15:31:12 -0300

    CHANGE, app title

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="/">amazona</a>
+            <a className="brand" href="/">Amazona</a>
           </div>
           <div>
             <a href="/cart">Cart</a>
