d72c5a3b6bcdf7fa713705f022b41cf1a428dd0c
Francisco Mackinlay <franmackinlay@gmail.com>
2021-01-07 15:30:18 -0300

    ADD, productDetails redux CARS

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,4 +1,4 @@
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from '../constants/productConstant'
+import { PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from '../constants/productConstant'
 import ProductsSrv from '../services/ProductsSrv';
 
 export const listProducts = () => async (dispatch) => {
@@ -11,5 +11,25 @@ export const listProducts = () => async (dispatch) => {
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: products });
   } catch (error) {
     dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message });
+  }
+}
+
+export const detailsProduct = productId => async dispatch => {
+  dispatch({
+    type: PRODUCT_DETAILS_REQUEST,
+    payload: productId,
+  });
+
+  try {
+    const product = await ProductsSrv.getProductDetails(productId);
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: product,
+    })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+    });
   }
 }
