074da8fcbc486311356807b9cbc9f463437ffd38
Francisco Mackinlay <franmackinlay@gmail.com>
2021-01-07 15:29:28 -0300

    ADD, ProductsSrv.getProductDetails

--- frontend/src/services/ProductsSrv.js
+++ frontend/src/services/ProductsSrv.js
@@ -4,6 +4,10 @@ const ProductsSrv = {
   getProducts: async () => {
     const { data } = await axios.get('/api/products');
     return data;
+  },
+  getProductDetails: async productId => {
+    const { data } = await axios.get(`/api/products/${productId}`);
+    return data;
   }
 }
 
