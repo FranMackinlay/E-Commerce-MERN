aac4fff644c2843946b45e8a8fffa923cee8b3d6
Francisco Mackinlay <franmackinlay@gmail.com>
2021-01-09 16:49:16 -0300

    FIX, ProductsSrv

--- frontend/src/services/ProductsSrv.js
+++ frontend/src/services/ProductsSrv.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 const ProductsSrv = {
   getProducts: async () => {
     const { data } = await axios.get('/api/products');
-    return data;
+    return data.products;
   },
   getProductDetails: async productId => {
     const { data } = await axios.get(`/api/products/${productId}`);
-    return data;
+    return data.product;
   }
 }
 
