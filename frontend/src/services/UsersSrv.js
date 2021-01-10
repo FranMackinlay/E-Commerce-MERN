30b4de86d8780b677fb307a1c832cbff1ea092b4
Francisco Mackinlay <franmackinlay@gmail.com>
2021-01-10 14:18:20 -0300

    ADD, UsersSrv

---
+++ frontend/src/services/UsersSrv.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+// const axios = require('axios');
+
+const UsersSrv = {
+  userSignIn: async (email, password) => {
+    const res = await axios.post('/api/users/signin', { email, password });
+    return res;
+  },
+}
+// const signIn = async () => {
+//   const res = await UsersSrv.userSignIn('admin@amazona.com', '1234');
+//   console.log('RES', res);
+// }
+
+// signIn();
+
+export default UsersSrv;
