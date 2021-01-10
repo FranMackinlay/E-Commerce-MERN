a082571a27ca5f5a46eb275ccf01571c01c1a1fb
Francisco Mackinlay <franmackinlay@gmail.com>
2021-01-10 14:18:29 -0300

    IMPROVE, SignInScreen

--- frontend/src/screens/SignInScreen.jsx
+++ frontend/src/screens/SignInScreen.jsx
@@ -1,16 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import {Link} from 'react-router-dom';
+import { signin } from '../actions/userActions';
+import LoadingBox from '../components/LoadingBox/LoadingBox';
+import MessageBox from '../components/MessageBox/MessageBox';
 
-export default function SignInScreen() {
+export default function SignInScreen(props) {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const userSignIn = useSelector(state => state.userSignIn);
+  const { userInfo, loading, error } = userSignIn;
+
+  const redirect = props.location.search ? props.location.search.split('=')[1] : '/';
+
+
+  const dispatch = useDispatch();
+
+
+
   const submitHandler = e => {
     e?.preventDefault();
-    // TODO: signin action
+    dispatch(signin(email, password));
   }
 
+  useEffect(() => {
+    if (userInfo) {
+      props.history.push(redirect);
+    };
+  }, [props.history, redirect, userInfo]);
 
   return (
     <div>
@@ -20,6 +39,8 @@ export default function SignInScreen() {
             Sign In
           </h1>
         </div>
+        {loading && <LoadingBox></LoadingBox>}
+        {error && <MessageBox variant="danger">{error}</MessageBox>}
         <div>
           <label htmlFor="email">Email address</label>
           <input type="email" name="" id="email" placeholder="Email" onChange={e => setEmail(e.target.value)} required/>
