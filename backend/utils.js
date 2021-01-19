4242f5226343bdc07a7b465aa68474a930f44c99
Francisco Mackinlay <franmackinlay@gmail.com>
2021-01-19 17:43:55 -0300

    CREATE, isAuth

--- backend/utils.js
+++ backend/utils.js
@@ -14,3 +14,18 @@ export const generateToken = user => {
     expiresIn: '30d',
   });
 };
+
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length);
+    jwt.verify(token, process.env.JWT_SECRET || 'somethingsecret', (err, decode) => {
+      if (err) return res.status(401).send({ message: 'Invalid token' });
+      req.user = decode;
+      next();
+    });
+  } else {
+    return res.status(401).send({ message: 'No token' });
+  }
+}
