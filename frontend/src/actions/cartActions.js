44842b3aced24c241af2351b2e6a5f7697734636
Francisco Mackinlay <franmackinlay@gmail.com>
2021-01-19 13:02:12 -0300

    ADD, paymentMethod CARS

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from '../constants/cartConstant';
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SHIPPING_ADDRESS } from '../constants/cartConstant';
 import ProductsSrv from '../services/ProductsSrv';
 
 
@@ -33,4 +33,10 @@ export const removeFromCart = productId => async (dispatch, getState) => {
 export const saveShippingAddress = data => dispatch => {
   dispatch({ type: CART_SAVE_SHIPPING_ADDRESS, payload: data });
   localStorage.setItem('shippingAddress', JSON.stringify(data));
+};
+
+export const savePaymentMethod = data => dispatch => {
+  dispatch({
+    type: CART_SAVE_PAYMENT_METHOD, payload: data
+  });
 };
