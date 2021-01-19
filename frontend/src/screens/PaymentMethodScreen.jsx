f12196a88525e558966ae24ff9a960f22cecb52d
Francisco Mackinlay <franmackinlay@gmail.com>
2021-01-19 15:37:17 -0300

    FIX, paymentMethodScreen

--- frontend/src/screens/PaymentMethodScreen.jsx
+++ frontend/src/screens/PaymentMethodScreen.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { savePaymentMethod } from '../actions/cartActions';
 import CheckoutSteps from '../components/CheckoutSteps/CheckoutSteps';
 
 const PaymentMethodScreen = props => {
+  const cart = useSelector(state => state.cart);
+  const { shippingAddress } = cart;
+  if (!shippingAddress.address) props.history.push('/shipping');
   const [paymentMethod, setPaymentMethod] = useState('PayPal');
   const dispatch = useDispatch();
 
@@ -11,7 +14,6 @@ const PaymentMethodScreen = props => {
     e?.preventDefault();
     dispatch(savePaymentMethod(paymentMethod));
     props.history.push('/placeorder')
-
   }
   return (
     <div>
@@ -22,14 +24,14 @@ const PaymentMethodScreen = props => {
         </div>
         <div>
           <div>
-            <input type="radio" id="paypal" value="PayPal" name="paymentMethod" requred checked onChange={e => setPaymentMethod(e.target.value)}/>
+            <input type="radio" id="paypal" value="PayPal" name="paymentMethod" required checked onChange={e => setPaymentMethod(e.target.value)}/>
             <label htmlFor="paypal">Paypal</label>
           </div>
         </div>
 
         <div>
           <div>
-            <input type="radio" id="stripe" value="Stripe" name="paymentMethod" requred onChange={e => setPaymentMethod(e.target.value)}/>
+            <input type="radio" id="stripe" value="Stripe" name="paymentMethod" required onChange={e => setPaymentMethod(e.target.value)}/>
             <label htmlFor="stripe">Stripe</label>
           </div>
         </div>
