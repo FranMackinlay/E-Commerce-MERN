a1b179062f98d44aa6f3d6d852bc7ad37efbc210
Francisco Mackinlay <franmackinlay@gmail.com>
2021-01-21 16:35:40 -0300

    ADD, user /profile route

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcryptjs';
 import expressAsyncHandler from 'express-async-handler';
 import data from '../data.js';
 import User from '../schemas/userModel.js';
-import { generateToken } from '../utils.js';
+import { generateToken, isAuth } from '../utils.js';
 
 const userRouter = express.Router();
 
@@ -56,6 +56,30 @@ userRouter.get('/:id', expressAsyncHandler(async (req, res) => {
 
   res.status(404).send({ message: 'User not found' });
 }));
+
+userRouter.put('/profile', isAuth, expressAsyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = bcrypt.hashSync(req.body.password, 8);
+    }
+    const updatedUser = await user.save();
+
+    return res.send({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      flags: {
+        isAdmin: updatedUser.flags.isAdmin,
+      },
+      token: generateToken(updatedUser),
+    });
+  }
+  res.status(404).send({ message: 'User not found' });
+}))
 
 
 export default userRouter;
